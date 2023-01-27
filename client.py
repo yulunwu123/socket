df14688d219434c7ef961b5cf69c8cdfb0d99298
yulunwu123 <yw9pz@virginia.edu>
2023-01-26 20:16:06 -0500

    server and client sockets

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ except IndexError:
     print("Please enter an integer between 1 and 100 as a command line argument")
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
+PORT = 56283  # Port to listen on (non-privileged ports are > 1023)
 NAME = "Client of Yulun Wu"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -18,9 +18,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.sendall(bytes(msg, "utf-8"))
     data = s.recv(1024)
 
-data_arr = data.decode("utf-8")
-data_arr = data_arr.split(",")
-server_name = data_arr[0]
-server_number = int(data_arr[1])
-print(f"The {NAME} sent {number} to the {server_name} and received an integer {server_number}. " \
-      f"Sum of values: {server_number + int(number)}.")
+    data_arr = data.decode("utf-8")
+    data_arr = data_arr.split(",")
+    server_name = data_arr[0]
+    server_number = int(data_arr[1])
+    print(f"The {NAME} sent {number} to the {server_name} and received an integer {server_number}. " \
+          f"Sum of values: {server_number + int(number)}.")
--- server.py
+++ server.py
@@ -1,33 +1,50 @@
-
 import random
 import socket
 
-
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
+PORT = 56283  # Port to listen on (non-privileged ports are > 1023)
 NAME = "Server of Yulun Wu"
 SERVER_NUMBER = random.randint(1, 100)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
 
-            msg_str = data.decode("utf-8")
-            msg_arr = msg_str.split(" ")
-            client_number = int(msg_arr[-1])
-            sum = client_number + SERVER_NUMBER
-            msg_arr.pop()  # remove the last element (number)
-            client_name = " ".join(msg_arr)
-            print(f"The {NAME} is communicating with the {client_name}")
-            print(f"Client's number: {client_number}")
-            print(f"Server's number: {SERVER_NUMBER}")
-            print(f"Sum: {sum}")
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
 
-            data_to_client = NAME + ", " + str(SERVER_NUMBER)  # sends server name & server's chosen # to client
-            conn.sendall(bytes(data_to_client, "utf-8"))
+                msg_str = data.decode("utf-8")
+                msg_arr = msg_str.split(" ")
+                client_number = int(msg_arr[-1])
+                sum = client_number + SERVER_NUMBER
+                msg_arr.pop()  # remove the last element (number)
+                client_name = " ".join(msg_arr)
+                print(f"The {NAME} is communicating with the {client_name}")
+                print(f"Client's number: {client_number}")
+                print(f"Server's number: {SERVER_NUMBER}")
+                print(f"Sum: {sum}")
+
+                data_to_client = NAME + ", " + str(SERVER_NUMBER)  # sends server name & server's chosen # to client
+                conn.sendall(bytes(data_to_client, "utf-8"))
+
+    # while True:
+    #     try:
+    #         clientsocket, address = s.accept()
+    #     except:
+    #         continue
+    #     print("Connection from", address[0])
+    #
+    #     with clientsocket:
+    #         while True:
+    #             data = clientsocket.recv(1024)
+    #             if not data:
+    #                 break
+    #             print("Is Communicating")
+
+
+
